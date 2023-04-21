4434194517587cb9309f0f1e716d1b10af660cb6
ksald <daniel@dlask.eu>
2023-04-21 12:06:13 +0000

    uz  je to  ok

---
+++ setup-arch-bash.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+sudo pacman -Syu
+sudo pacman -S lz4
+sudo pacmna -S  manpages-dev
+echo 'alias xdd=~/.xdd/run.sh' >> ~/.bashrc
+wget -P ~/.xdd https://raw.githubusercontent.com/MartinRostejnsky/xddCompress/main/src/run.sh
+wget -P ~/.xdd https://github.com/MartinRostejnsky/xddCompress/raw/main/src/a.out
+wget -P ~/.xdd https://raw.githubusercontent.com/MartinRostejnsky/xddCompress/main/src/decode.sh
+wget -P ~/.xdd https://raw.githubusercontent.com/MartinRostejnsky/xddCompress/main/src/encode.sh
+chmod u+x ~/.xdd/run.sh
+chmod u+x ~/.xdd/a.out
+chmod u+x ~/.xdd/decode.sh
+chmod u+x ~/.xdd/encode.sh
