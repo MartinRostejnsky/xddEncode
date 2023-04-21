7f9c7215c0b71d94b1bb3ced1cd9967693d98967
ksald <daniel@dlask.eu>
2023-04-21 08:58:12 +0000

    mozna to funguje

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-sudo apt update
-sudo apt install lz4
-sudo apt-get install manpages-dev
-echo 'alias xdd=~/xdd/run.sh' >> ~/.bashrc
+sudo pacman -Syu
+sudo pacman -S lz4
+sudo pacmna -S  manpages-dev
+echo 'alias xdd=~/xdd/run.sh' >> ~/.zshrc
 wget -P ~/xdd https://raw.githubusercontent.com/MartinRostejnsky/xddCompress/main/src/run.sh
 wget -P ~/xdd https://github.com/MartinRostejnsky/xddCompress/raw/main/src/a.out
 wget -P ~/xdd https://raw.githubusercontent.com/MartinRostejnsky/xddCompress/main/src/decode.sh
